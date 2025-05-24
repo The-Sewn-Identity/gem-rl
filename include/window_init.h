a313b05545f004958855ce97dfcc44d67e8a610d
The Sewn Identity <179808961+The-Sewn-Identity@users.noreply.github.com>
2025-05-24 22:29:30 +0200

    3D Camera added

---
+++ include/window_init.h
@@ -0,0 +1,11 @@
+#ifndef WINDOW_INIT_H
+#define WINDOW_INIT_H
+
+#define RENDER_WIDTH 848
+#define RENDER_HEIGHT 480
+
+#define DEFAULT_FPS 120
+
+#define TITLE "raylib Window"
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,17 +1,31 @@
 #include <raylib.h>
 #include <resource_dir.h>
+#include <rcamera.h>
+
+#include "window_init.h"
 
 int main() {
-	InitWindow(848, 480, "raylib Window");
-	SetTargetFPS(120);
+	InitWindow(848, 480, TITLE);
+	SetTargetFPS(DEFAULT_FPS);
+
+	Camera3D camera = {0};
+	camera.position = (Vector3){0, 10.0f, 10.0f};
+	camera.target = (Vector3){0, 0.0f, 0};
+	camera.up = (Vector3){0, 1.0f, 0};
+	camera.fovy = 45.0f;
+	camera.projection = CAMERA_PERSPECTIVE;
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-			ClearBackground(WHITE);
+			ClearBackground(RAYWHITE);
+			
+			BeginMode3D(camera);
+				DrawGrid(10, 1.0f);
+			EndMode3D();
 
 		EndDrawing();
 	}
-	
+
 	CloseWindow();
 
 	return 0;
